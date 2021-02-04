72dcde2368dfebeb03a1c2c92e39c53d696548e6
weanew <weanew@yandex.ru>
2021-02-04 18:52:46 +0300

    added parallelization

--- talkingImage/generator.py
+++ talkingImage/generator.py
@@ -1,21 +1,22 @@
-from talkingImage.detect import LandmarkFace
-from talkingImage.transformation import LipsTransform
-import matplotlib.pyplot as plt
-from spleeter.separator import Separator
-from spleeter.audio.adapter import AudioAdapter
-import numpy as np
+import os
+import uuid
+from multiprocessing import Process, Manager
+
 import cv2
 import ffmpeg
-import uuid
-import os
+import matplotlib.pyplot as plt
+import numpy as np
+from spleeter.audio.adapter import AudioAdapter
+from spleeter.separator import Separator
+
+from talkingImage.detect import LandmarkFace
+from talkingImage.transformation import LipsTransform
 
 
 def generate(image_path, music_path, output_path):
-
     decimals = 1
     FPS = 30
     sample_rate = 44100
-
 
     def generate_images(decimals, lipsT):
         imgs = {}
@@ -24,16 +25,17 @@ def generate(image_path, music_path, output_path):
             imgs[str(coef)] = lipsT.transformate(coef)
         return imgs
 
-    def image_processing(image_pth):
+    def image_processing(image_pth, ret_value):
         img = plt.imread(image_pth)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (1024, 1024), interpolation=cv2.INTER_LINEAR)
         lface = LandmarkFace.estimate(img)
         lips = LipsTransform(lface)
         imgs = generate_images(decimals, lips)
+        ret_value['images'] = imgs
         return imgs
 
-    def music_processing(music_pth):
+    def music_processing(music_pth, ret_value):
         separator = Separator(params_descriptor='spleeter:2stems')
 
         audio_adapter = AudioAdapter.get('spleeter.audio.ffmpeg.FFMPEGProcessAudioAdapter')
@@ -44,11 +46,26 @@ def generate(image_path, music_path, output_path):
         )
         sources = separator.separate(waveform=waveform, audio_descriptor=music_pth)
         vocals = sources['vocals']
+        ret_value['vocals'] = vocals
         return vocals
 
+    manager = Manager()
+    ret_dict = manager.dict()
+    procs = [
+        Process(target=music_processing, args=(music_path, ret_dict)),
+        Process(target=image_processing, args=(image_path, ret_dict))
+    ]
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
+
+    voc = ret_dict['vocals']
+    images = ret_dict['images']
+
     out = cv2.VideoWriter('temp_video.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), FPS, (1024, 1024))
-    voc = music_processing(music_path)
-    images = image_processing(image_path)
     time = voc.shape[0] * voc.shape[1] / sample_rate
     frames_count = time * FPS
 
